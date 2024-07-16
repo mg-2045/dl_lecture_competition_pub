1cfa268295a79900d712c0fc2e3f93e9522e34c6
mg-2045 <nsx_mo@yahoo.co.jp>
2024-07-17 04:37:21 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ from pathlib import Path
 from typing import Dict, Any
 import os
 import time
-
+from torchvision import transforms
+from torch.cuda.amp import GradScaler, autocast
+import torch.nn as nn # Import the necessary module
+import torch.optim as optim # Import the torch.optim module
+import torchvision.transforms.functional as F
 
 class RepresentationType(Enum):
     VOXEL = auto()
@@ -70,10 +74,14 @@ def main(args: DictConfig):
             ├─zurich_city_11_b
             └─zurich_city_11_c
         '''
-    
+
     # ------------------
     #    Dataloader
     # ------------------
+    
+    #EventDataset = EventData(args.data_path, 'train')
+    #EventDataLoader = torch.utils.data.DataLoader(dataset=EventDataset, batch_size=args.batch_size, shuffle=True)
+    
     loader = DatasetProvider(
         dataset_path=Path(args.dataset_path),
         representation_type=RepresentationType.VOXEL,
@@ -101,7 +109,7 @@ def main(args: DictConfig):
         Key: event_volume, Type: torch.Tensor, Shape: torch.Size([Batch, 4, 480, 640]) => イベントデータのバッチ
         Key: flow_gt, Type: torch.Tensor, Shape: torch.Size([Batch, 2, 480, 640]) => オプティカルフローデータのバッチ
         Key: flow_gt_valid_mask, Type: torch.Tensor, Shape: torch.Size([Batch, 1, 480, 640]) => オプティカルフローデータのvalid. ベースラインでは使わない
-    
+
     test data:
         Type of batch: Dict
         Key: seq_name, Type: list
@@ -116,55 +124,121 @@ def main(args: DictConfig):
     #   optimizer
     # ------------------
     optimizer = torch.optim.Adam(model.parameters(), lr=args.train.initial_learning_rate, weight_decay=args.train.weight_decay)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, verbose=True) #add
+
     # ------------------
     #   Start training
     # ------------------
+    #model_path = "/content/drive/MyDrive/Colab Notebooks/DLBasics2023_colab/end/EventCamera/checkpoints/model_33_20240706182716.pth"
+    #model.load_state_dict(torch.load(model_path, map_location=device))   #add
+
+    print("all epoch: {}".format(args.train.epochs))
     model.train()
+    scaler = torch.cuda.amp.GradScaler(init_scale=10000)  #add
+
     for epoch in range(args.train.epochs):
-        total_loss = 0
+        total_loss0 = 0
+        total_loss1 = 0
+        total_loss2 = 0
+        loss_batches = 0
+
         print("on epoch: {}".format(epoch+1))
+
+        transform = transforms.Compose([transforms.RandomHorizontalFlip(p=1.0)]) #add
+        #transform2 = transforms.Compose([transforms.RandomVerticalFlip(p=1.0)]) #add
+        #transform = transforms.Compose([transforms.RandomErasing(p=0.8, scale=(0.02, 0.33), ratio=(0.3, 3.3)),]) # random erasing
+
         for i, batch in enumerate(tqdm(train_data)):
             batch: Dict[str, Any]
+
+            #通常画像
             event_image = batch["event_volume"].to(device) # [B, 4, 480, 640]
             ground_truth_flow = batch["flow_gt"].to(device) # [B, 2, 480, 640]
-            flow = model(event_image) # [B, 2, 480, 640]
+            optimizer.zero_grad()   #add
+            
+            flow_dict = model(event_image) # [B, 2, 480, 640]
+
+            flow_dict['flow0'] = F.resize(img=flow_dict['flow0'], size=(480,640))
+            flow_dict['flow1'] = F.resize(img=flow_dict['flow1'], size=(480,640))            
+            flow_dict['flow2'] = F.resize(img=flow_dict['flow2'], size=(480,640))  
+            flow = (flow_dict['flow0']*1.2+flow_dict['flow1']*2+flow_dict['flow2']*3+flow_dict['flow3']*4)/10.2 #14.159 
             loss: torch.Tensor = compute_epe_error(flow, ground_truth_flow)
-            print(f"batch {i} loss: {loss.item()}")
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
 
-            total_loss += loss.item()
-        print(f'Epoch {epoch+1}, Loss: {total_loss / len(train_data)}')
+            print(f"batch {i} loss1: {loss.item()}")
+            scaler.scale(loss).backward() # 勾配の消失を防ぐために、GradScalerを使用して逆伝播。
+            scaler.step(optimizer) # パラメータを更新
+            scaler.update() # GradScalerを更新
+            total_loss1 += loss.item()
+            loss_batches += 1 #add
 
-    # Create the directory if it doesn't exist
-    if not os.path.exists('checkpoints'):
-        os.makedirs('checkpoints')
-    
-    current_time = time.strftime("%Y%m%d%H%M%S")
-    model_path = f"checkpoints/model_{current_time}.pth"
-    torch.save(model.state_dict(), model_path)
-    print(f"Model saved to {model_path}")
+        scheduler.step(total_loss0/loss_batches) #add
+        loss_batches=0
 
-    # ------------------
-    #   Start predicting
-    # ------------------
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    model.eval()
-    flow: torch.Tensor = torch.tensor([]).to(device)
-    with torch.no_grad():
-        print("start test")
-        for batch in tqdm(test_data):
+        for i, batch in enumerate(tqdm(train_data)):
             batch: Dict[str, Any]
-            event_image = batch["event_volume_old"].to(device)
-            batch_flow = model(event_image) # [1, 2, 480, 640]
-            flow = torch.cat((flow, batch_flow), dim=0)  # [N, 2, 480, 640]
-        print("test done")
-    # ------------------
-    #  save submission
-    # ------------------
-    file_name = "submission.npy"
-    save_optical_flow_to_npy(flow, file_name)
+
+            # Flipping HorizontalFlip
+            batch['event_volume'] = transform(batch['event_volume'])#add
+            event_image = batch["event_volume"].to(device) # [B, 4, 480, 640]
+            ground_truth_flow = batch["flow_gt"].to(device) # [B, 2, 480, 640]
+            optimizer.zero_grad()   #add
+            
+            flow_dict = model(event_image) # [B, 2, 480, 640]
+            
+            flow_dict['flow0'] = F.resize(img=flow_dict['flow0'], size=(480,640)) #中間LOSS計算用
+            flow_dict['flow1'] = F.resize(img=flow_dict['flow1'], size=(480,640)) #中間LOSS計算用          
+            flow_dict['flow2'] = F.resize(img=flow_dict['flow2'], size=(480,640)) #中間LOSS計算用 
+            flow = (flow_dict['flow0']*1.2+flow_dict['flow1']*2+flow_dict['flow2']*3+flow_dict['flow3']*4)/10.2 #14.159 #中間LOSS計算用
+            loss: torch.Tensor = compute_epe_error(flow, ground_truth_flow)
+
+            print(f"batch {i} loss2: {loss.item()}")
+            scaler.scale(loss).backward() # 勾配の消失を防ぐために、GradScalerを使用して逆伝播。
+            scaler.step(optimizer) # パラメータを更新
+            scaler.update() # GradScalerを更新
+            total_loss2 += loss.item()
+            loss_batches += 1 #add
+
+        scheduler.step(total_loss0/loss_batches) #0711_0800
+        total_loss0 = (total_loss1 + total_loss2) / 2 #0711_0800
+
+        print(f'Epoch {epoch+1}, Loss1: {total_loss1 / len(train_data)}')
+        print(f'Epoch {epoch+1}, Loss2: {total_loss2 / len(train_data)}')
+        print(f'Epoch {epoch+1}, mean_Loss: {total_loss0 / len(train_data)}')
+
+        # Create the directory if it doesn't exist
+        if not os.path.exists('checkpoints'):
+            os.makedirs('checkpoints')
+
+        current_time = time.strftime("%Y%m%d%H%M%S")
+        model_path = f"checkpoints/model_11_{current_time}.pth"
+        torch.save(model.state_dict(), model_path)
+        print(f"Model saved to {model_path}")
+
+        # ------------------
+        #   Start predicting
+        # ------------------
+        model.load_state_dict(torch.load(model_path, map_location=device))
+        model.eval()
+        flow: torch.Tensor = torch.tensor([]).to(device)
+        with torch.no_grad():
+            print("start test")
+            for batch in tqdm(test_data):
+                batch: Dict[str, Any]
+                event_image = batch["event_volume"].to(device)
+                flow_dict2 = model(event_image) # [1, 2, 480, 640]
+
+                flow_dict2['flow0'] = F.resize(img=flow_dict2['flow0'], size=(480,640))
+                flow_dict2['flow1'] = F.resize(img=flow_dict2['flow1'], size=(480,640))            
+                flow_dict2['flow2'] = F.resize(img=flow_dict2['flow2'], size=(480,640))
+                flow_dict3 = (flow_dict2['flow0']*1.2+flow_dict2['flow1']*2+flow_dict2['flow2']*3+flow_dict2['flow3']*4)/10.2                
+                
+                flow = torch.cat((flow,flow_dict3), dim=0)  # [N, 2, 480, 640]
+            print("test done")
+        # ------------------
+        #  save submission
+        # ------------------
+        file_name = 'submission_11_'+'{0:03d}'.format(epoch)
+        save_optical_flow_to_npy(flow, file_name)
 
 if __name__ == "__main__":
     main()
